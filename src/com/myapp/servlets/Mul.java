97f96acc8f370ee356bcce67c994dc7d2db04ed4
Denus Melnychenko <aspirant89@inbox.ru>
2017-03-29 01:32:18 +0300

    Created URL calculator using servlets

---
+++ src/com/myapp/servlets/Mul.java
@@ -0,0 +1,25 @@
+package com.myapp.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/mul")
+public class Mul extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		int num1 = Integer.parseInt(request.getParameter("num1"));
+		int num2 = Integer.parseInt(request.getParameter("num2"));
+		int mul = num1 * num2;
+		out.append("Differense = " + mul);
+	}
+
+}
