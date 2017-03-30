7111e12b9fee6077fe40c4fc3bf8b0fb3eb63430
Denus Melnychenko <aspirant89@inbox.ru>
2017-03-31 00:17:58 +0300

    Added Add servlet

---
+++ src/com/myapp/servlets/Add.java
@@ -0,0 +1,25 @@
+package com.myapp.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/add")
+public class Add extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		int num1 = Integer.parseInt(request.getParameter("num1"));
+		int num2 = Integer.parseInt(request.getParameter("num2"));
+		int sum = num1 + num2;
+		out.append("Summa = " + sum);
+	}
+
+}
